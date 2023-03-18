79468158b4fe8be75371c001f9245b6a071b8f36
Arthur Augusto <arthuraugusto.exe@gmail.com>
2023-03-18 15:28:13 -0300

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      {' '}
+      <p>© Arthur Augusto 2023</p>{' '}
+    </footer>
+  );
+};
+
+export default Footer;
